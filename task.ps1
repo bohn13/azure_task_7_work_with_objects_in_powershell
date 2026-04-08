ead3fffdf032a9b85b4b49e086dedf3ea81211e4
bohn13 <bod3171@gmail.com>
2026-04-08 18:30:46 +0200

    Completed the task

--- task.ps1
+++ task.ps1
@@ -1,1 +1,21 @@
-# Write your code here
+$files = Get-ChildItem -Path .\data -Filter *.json
+
+$TargetVM = "Standard_B2pts_v2"
+
+$regions = @()
+
+foreach($file in $files) {
+    $json = Get-Content -Path $file.FullName -Raw | ConvertFrom-Json
+
+    $foundVm = $json | Where-Object { $_.name -eq $TargetVM}
+
+    if ($foundVm) {
+        Write-Host "Found VM: $($foundVm.name) in file: $($file.FullName)"
+        $regions += $file.BaseName
+    } else {
+        Write-Host "VM: $TargetVM not found in file: $($file.FullName)"
+    }
+
+}
+
+$regions | ConvertTo-Json | Out-File -FilePath ./result.json
